9a73a595f87d85074f9a7aa183baaa70d142f449
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2022-09-08 17:51:18 +0200

    [#37034] Add STM32_ADC

--- dts2repl/dts2repl.py
+++ dts2repl/dts2repl.py
@@ -364,7 +364,7 @@ def generate(args):
             # decide which IRQ names to use in Renode model
             if compat == 'st,stm32-rtc':
                 irq_names = ['AlarmIRQ']
-            elif compat in ['nxp,kinetis-lpuart', 'nxp,kinetis-uart', 'silabs,gecko-leuart', 'sifive,uart0']:
+            elif compat in ['nxp,kinetis-lpuart', 'nxp,kinetis-uart', 'silabs,gecko-leuart', 'sifive,uart0', 'st,stm32-adc']:
                 irq_names = ['IRQ']
             elif compat in ['silabs,gecko-uart', 'silabs,gecko-usart']:
                 irq_names = ['ReceiveIRQ', 'TransmitIRQ']
