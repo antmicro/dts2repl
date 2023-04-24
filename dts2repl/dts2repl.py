9e7e177adc37c294c5ce7477bb773e41dd367c3d
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2023-04-24 23:03:31 +0200

    [#43921] Support #interrupt-cells in get_node_prop

--- dts2repl/dts2repl.py
+++ dts2repl/dts2repl.py
@@ -155,7 +155,8 @@ def get_node_prop(node, prop, default=None, inherit=False):
         val = val.to_strings()
     elif prop in ('interrupts', 'reg', 'ranges'):
         val = val.to_nums()
-    elif prop in ('#address-cells', '#size-cells', 'cc-num', 'clock-frequency', 'riscv,ndev'):
+    elif prop in ('#address-cells', '#size-cells', '#interrupt-cells', 'cc-num', 'clock-frequency',
+                  'riscv,ndev'):
         val = val.to_num()
     elif prop in ('interrupt-parent',):
         val = val.to_node()
